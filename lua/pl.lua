55ef9be772c25b18b14367b47de6a3b71df72ad5
Myaamori <myaamori1993@gmail.com>
2019-04-24 00:48:37 +0100

    Add DependencyControl record

---
+++ lua/pl.lua
@@ -0,0 +1,11 @@
+local DependencyControl = require("l0.DependencyControl")
+local version = DependencyControl{
+    name = "Penlight",
+    version = "1.6.0",
+    description = "Python-inspired utility library.",
+    author = "stevedonovan",
+    url = "http://github.com/Myaamori/Penlight",
+    moduleName = "myaa.pl",
+}
+
+return {version=version}
